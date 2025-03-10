5184ed3794d124e322ff0a17b3af4e3fa01b7bf2
linong <lilnong1@126.com>
2025-03-10 21:24:54 +0800

    feat(Portal): add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    apps: [
+        {
+            name: 'home-2025-dev',
+            script: 'node_modules/.bin/next',
+            args: 'dev',
+            cwd: './packages/web',
+            watch: ['src'],
+            env: {
+                NODE_ENV: 'development',
+                PORT: 3000,
+            },
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: 3000,
+            },
+            pre_setup: 'pnpm install'
+        },
+        {
+            name: 'home-2025-prod',
+            script: 'node_modules/.bin/next',
+            args: 'start',
+            cwd: './packages/web',
+            instances: 'max',
+            exec_mode: 'cluster',
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: 3000,
+            },
+            pre_setup: 'pnpm install && pnpm build'
+        },
+    ],
+}; 
